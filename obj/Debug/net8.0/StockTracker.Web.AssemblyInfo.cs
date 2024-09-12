f6eb5209dd8736199252347ed94fa8a946e2f7c9
brockmortenson <mortensonbrock@gmail.com>
2024-09-11 20:48:56 -0600

    setup

--- obj/Debug/net8.0/StockTracker.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/StockTracker.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockTracker.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbb50d6a4056b8a5a8b4248092680025134d99e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockTracker.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockTracker.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
